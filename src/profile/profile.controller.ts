bd4ce01822b0903d7a467cd5857276884a32b94d
Ekene Agwu <agwuekene41@gmail.com>
2022-10-28 07:02:07 +0100

    small changes

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -5,6 +5,6 @@ export class ProfileController {
     
     @Get()
     async profile(){
-        return { slackUsername: 'Kenn-stack', backend: true, age: 20, bio: "Hello, I'm a backend developer" }
+        return { slackUsername: 'agwuekene41', backend: true, age: 20, bio: "Hello, I'm a backend developer" }
     }
 }

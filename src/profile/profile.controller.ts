d8ad6da915032d70ccdc394b57537713b7f5493d
Ekene Agwu <agwuekene41@gmail.com>
2022-11-05 05:30:16 +0100

    fix bugs

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -14,10 +14,10 @@ export class ProfileController {
     async arithmetic(@Req() req: Request){
         let result:number
         if(req.body.operation_type == 'addition'){
-            return { slackUsername: 'agwuekene41', result: req.body.x + req.body.y, operation_type : req.body.operation_type}
+            return { slackUsername: 'agwuekene41', result: parseInt(req.body.x) + parseInt(req.body.y), operation_type : req.body.operation_type}
         }
-        else if(req.body.operation_type == 'subtraction'){ result = req.body.x - req.body.y}
-        else if(req.body.operation_type == 'multiplication'){ result = req.body.x * req.body.y}
+        else if(req.body.operation_type == 'subtraction'){ result = parseInt(req.body.x) - parseInt(req.body.y)}
+        else if(req.body.operation_type == 'multiplication'){ result = parseInt(req.body.x) * parseInt(req.body.y)}
         return { slackUsername: 'agwuekene41', result: result, operation_type : req.body.operation_type}
 
     }
